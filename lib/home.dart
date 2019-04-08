a645ceb0118257c50233bc253d2985bfd4e281b8
peter_nuell <peternuell@hotmail.com>
2019-04-08 20:19:48 +0000

    Codelabs - Flutter tutorial - Shrine App
    Pre Products Repository

--- lib/home.dart
+++ lib/home.dart
@@ -13,9 +13,44 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
+
   // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(int count) {
+
+
+    return List.generate(
+      count,
+      (int index) => Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                  aspectRatio: 18.0 / 11.0,
+                  child: Image.asset('assets/diamond.png')
+              ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondary text')
+                      ]
+                  )
+              )
+            ]
+        )
+      )
+    );
+  }
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -36,19 +71,32 @@ class HomePage extends StatelessWidget {
         title: Text('SHRINE'),
         actions: <Widget>[
           IconButton(
-              icon: null,
-              onPressed: null
+              icon: Icon(
+                Icons.search,
+                semanticLabel: 'search',
+              ),
+              onPressed: () {
+                print('Search button');
+              }
           ),
           IconButton(
-              icon: Icon(),
-              onPressed: null
+              icon: Icon(
+                Icons.tune,
+                semanticLabel: 'filter'
+              ),
+              onPressed: () {
+                print('Filter button');
+              }
           )
         ]
       ),
       // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
-      ),
+      body: GridView.count(
+          crossAxisCount: 2,
+        padding: EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(10)
+      )
     );
   }
 }
