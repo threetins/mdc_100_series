9b5ccad0db510fc3b87cc8fa31cf039516bed871
peter_nuell <peternuell@hotmail.com>
2019-04-08 21:14:34 +0000

    Codelabs - Flutter tutorial - Shrine App
    Products Repository started

--- lib/home.dart
+++ lib/home.dart
@@ -21,35 +21,56 @@ import 'model/product.dart';
 class HomePage extends StatelessWidget {
 
   // TODO: Make a collection of cards (102)
-  List<Card> _buildGridCards(int count) {
+  List<Card> _buildGridCards(BuildContext context) {
 
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
 
-    return List.generate(
-      count,
-      (int index) => Card(
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString()
+    );
+
+    return products.map((product) {
+      return Card(
         clipBehavior: Clip.antiAlias,
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png')
-              ),
-              Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Title'),
-                        SizedBox(height: 8.0),
-                        Text('Secondary text')
-                      ]
-                  )
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage
               )
-            ]
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      product.name,
+                      style: theme.textTheme.title,
+                      maxLines: 1
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.body2
+                    )
+                  ]
+                )
+              ),
+            )
+          ]
         )
-      )
-    );
+      );
+    }).toList();
   }
   // TODO: Add a variable for Category (104)
   @override
@@ -95,7 +116,7 @@ class HomePage extends StatelessWidget {
           crossAxisCount: 2,
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        children: _buildGridCards(10)
+        children: _buildGridCards(context)
       )
     );
   }
